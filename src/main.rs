d15feb9d58b1cdd1e35f14245dee04f9e0efa0f4
Andrew Fitzgerald <apfitzge@gmail.com>
2023-11-29 18:03:41 +0000

    Set up basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use {clap::Parser, solana_sdk::clock::Slot};
+
+#[derive(Debug, Parser)]
+struct Cli {
+    /// Slot to fetch block and perform priority checks for.
+    slot: Slot,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("Args: {cli:?}");
 }
